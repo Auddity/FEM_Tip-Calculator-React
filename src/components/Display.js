7b3c926e23fd08671c6a37699f78f22486cb9b67
Auddity <audsecondarylap@outlook.com>
2022-07-20 13:13:59 -0400

    display styles

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,15 +1,28 @@
 import React from 'react'
+import '../css/Display.css';
 
 function Display({ total, tipPerPerson, formatter }) {
   return (
     <div className='Display'>
-      <p>
-        {tipPerPerson !=='' && formatter.format(tipPerPerson)}
-      </p>
-      
-      <p>
-        {total !=='' && formatter.format(total)}
-      </p>
+      <div className="valueCtnr">
+        <p>Tip Amount
+          <br/>
+          <span>/ person</span></p>
+        <p className='value'>
+          {tipPerPerson !=='' && formatter.format(tipPerPerson)}
+          $4.27
+        </p>
+      </div>
+      <div className="valueCtnr">
+        <p>Total 
+          <br/>
+          <span>/ person</span></p>
+        <p className='value'>
+          {total !=='' && formatter.format(total)}
+          $32.79
+        </p>
+      </div>
+      <button className="resetBtn">RESET</button>
     </div>
   )
 }
